ef5550dd79df3b9a1a69ec9317b71041e5c01256
Olexandr25 <oleksandrvoichyshyn@gmail.com>
2021-08-27 12:27:23 +0300

    Wrote game and function clicking in script.js

--- dist/script.js
+++ dist/script.js
@@ -1,191 +1,203 @@
 const config = {
+  video: {
+    width: 640,
+    height: 480,
+    fps: 30,
+  },
+};
+
+const landmarkColors = {
+  thumb: "red",
+  indexFinger: "blue",
+  middleFinger: "yellow",
+  ringFinger: "green",
+  pinky: "pink",
+  palmBase: "white",
+};
+
+const gestureStrings = {
+  thumbs_up: "👍",
+  thumbs_down: "👎",
+  phone: "🤙",
+  finger_down: "👇",
+  finger_left: "👈",
+  finger_right: "👉",
+  fist: "✊",
+  okay: "👌",
+  love_you: "🤟",
+  two_down: "🐶",
+  three_down: "🐱",
+  four_down: "🦊",
+  five_down: "🐸",
+  one_up: "👆",
+  two_up: "✌🏻",
+  three_up: "🎲",
+  four_up: "🎯",
+};
+
+async function main() {
+  const video = document.querySelector("#pose-video");
+  const canvas = document.querySelector("#pose-canvas");
+  const ctx = canvas.getContext("2d");
+
+  const resultLayer = document.querySelector("#pose-result");
+
+  const knownGestures = [
+    fp.Gestures.ThumbsUpGesture,
+    fp.Gestures.ThumbsDownGesture,
+    fp.Gestures.PhoneGesture,
+    fp.Gestures.FingerDownGesture,
+    fp.Gestures.FingerLeftGesture,
+    fp.Gestures.FingerRightGesture,
+    fp.Gestures.FistGesture,
+    fp.Gestures.OkayGesture,
+    fp.Gestures.LoveYouGesture,
+    fp.Gestures.TwoDown,
+    fp.Gestures.ThreeDown,
+    fp.Gestures.FourDown,
+    fp.Gestures.FiveDown,
+    fp.Gestures.OneUpGesture,
+    fp.Gestures.TwoUpGesture,
+    fp.Gestures.ThreeUpGesture,
+    fp.Gestures.FourUpGesture,
+  ];
+  const GE = new fp.GestureEstimator(knownGestures);
+
+  // load handpose model
+  const model = await handpose.load();
+  console.log("Handpose model loaded");
+
+  // main estimation loop
+  const estimateHands = async () => {
+    // clear canvas overlay
+    ctx.clearRect(0, 0, config.video.width, config.video.height);
+    resultLayer.innerText = "";
+
+    // get hand landmarks from video
+    // Note: Handpose currently only detects one hand at a time
+    // Therefore the maximum number of predictions is 1
+    const predictions = await model.estimateHands(video, true);
+
+    for (let i = 0; i < predictions.length; i++) {
+      // draw colored dots at each predicted joint position
+      for (let part in predictions[i].annotations) {
+        for (let point of predictions[i].annotations[part]) {
+          drawPoint(ctx, point[0], point[1], 3, landmarkColors[part]);
+        }
+      }
+
+      // now estimate gestures based on landmarks
+      // using a minimum confidence of 7.5 (out of 10)
+      const est = GE.estimate(predictions[i].landmarks, 7.5);
+
+      if (est.gestures.length > 0) {
+        // find gesture with highest confidence
+        let result = est.gestures.reduce((p, c) => {
+          return p.confidence > c.confidence ? p : c;
+        });
+
+        resultLayer.innerText = gestureStrings[result.name];
+        scrollWindow(result.name);
+        zoomWindow(result.name);
+
+        btnClicker(result.name);
+      }
+    }
+
+    // ...and so on
+    setTimeout(() => {
+      estimateHands();
+    }, 1000 / config.video.fps);
+  };
+
+  estimateHands();
+  console.log("Starting predictions");
+}
+
+async function initCamera(width, height, fps) {
+  const constraints = {
+    audio: false,
     video: {
-      width: 640,
-      height: 480,
-      fps: 30
-    }
+      facingMode: "user",
+      width: width,
+      height: height,
+      frameRate: {
+        max: fps,
+      },
+    },
   };
 
-  const landmarkColors = {
-    thumb: 'red',
-    indexFinger: 'blue',
-    middleFinger: 'yellow',
-    ringFinger: 'green',
-    pinky: 'pink',
-    palmBase: 'white'
-  };
+  const video = document.querySelector("#pose-video");
+  video.width = width;
+  video.height = height;
 
-  const gestureStrings = {
-    'thumbs_up': '👍',
-    'thumbs_down': '👎',
-    'phone': '🤙',
-    'finger_down': '👇',
-    'finger_left': '👈',
-    'finger_right': '👉',
-    'fist' : '✊',
-    'okay' : '👌',
-    'love_you' : '🤟',
-    'two_down' : '🐶',
-    'three_down' : '🐱',
-    'four_down' : '🦊',
-    'five_down' : '🐸',
-    'one_up': '👆',
-    'two_up': '✌🏻',
-    'three_up': '🎲',
-    'four_up': '🎯'
-  };
+  // get video stream
+  const stream = await navigator.mediaDevices.getUserMedia(constraints);
+  video.srcObject = stream;
 
-  async function main() {
-
-    const video = document.querySelector("#pose-video");
-    const canvas = document.querySelector("#pose-canvas");
-    const ctx = canvas.getContext("2d");
-
-    const resultLayer = document.querySelector("#pose-result");
-
-    const knownGestures = [
-      fp.Gestures.ThumbsUpGesture,
-      fp.Gestures.ThumbsDownGesture,
-      fp.Gestures.PhoneGesture,
-      fp.Gestures.FingerDownGesture,
-      fp.Gestures.FingerLeftGesture,
-      fp.Gestures.FingerRightGesture,
-      fp.Gestures.FistGesture,
-      fp.Gestures.OkayGesture,
-      fp.Gestures.LoveYouGesture,
-      fp.Gestures.TwoDown,
-      fp.Gestures.ThreeDown,
-      fp.Gestures.FourDown,
-      fp.Gestures.FiveDown,
-      fp.Gestures.OneUpGesture,
-      fp.Gestures.TwoUpGesture,
-      fp.Gestures.ThreeUpGesture,
-      fp.Gestures.FourUpGesture
-    ];
-    const GE = new fp.GestureEstimator(knownGestures);
-
-    // load handpose model
-    const model = await handpose.load();
-    console.log("Handpose model loaded");
-
-    // main estimation loop
-    const estimateHands = async () => {
-
-      // clear canvas overlay
-      ctx.clearRect(0, 0, config.video.width, config.video.height);
-      resultLayer.innerText = '';
-
-      // get hand landmarks from video
-      // Note: Handpose currently only detects one hand at a time
-      // Therefore the maximum number of predictions is 1
-      const predictions = await model.estimateHands(video, true);
-
-      for (let i = 0; i < predictions.length; i++) {
-
-        // draw colored dots at each predicted joint position
-        for (let part in predictions[i].annotations) {
-          for (let point of predictions[i].annotations[part]) {
-            drawPoint(ctx, point[0], point[1], 3, landmarkColors[part]);
-          }
-        }
-
-        // now estimate gestures based on landmarks
-        // using a minimum confidence of 7.5 (out of 10)
-        const est = GE.estimate(predictions[i].landmarks, 7.5);
-
-        if (est.gestures.length > 0) {
-
-          // find gesture with highest confidence
-          let result = est.gestures.reduce((p, c) => {
-            return (p.confidence > c.confidence) ? p : c;
-          });
-
-          resultLayer.innerText = gestureStrings[result.name];
-          scrollWindow(result.name);
-          zoomWindow(result.name);
-        }
-      }
-
-      // ...and so on
-      setTimeout(() => {
-        estimateHands();
-      }, 1000 / config.video.fps);
+  return new Promise((resolve) => {
+    video.onloadedmetadata = () => {
+      resolve(video);
     };
+  });
+}
 
-    estimateHands();
-    console.log("Starting predictions");
+function drawPoint(ctx, x, y, r, color) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
+let count = 0;
+function btnClicker(resultName) {
+  if ("two_up" == resultName) {
+    document.getElementById("scissors").click();
+    console.log(count++);
   }
-
-  async function initCamera(width, height, fps) {
-
-    const constraints = {
-      audio: false,
-      video: {
-        facingMode: "user",
-        width: width,
-        height: height,
-        frameRate: {
-          max: fps
-        }
-      }
-    };
-
-    const video = document.querySelector("#pose-video");
-    video.width = width;
-    video.height = height;
-
-    // get video stream
-    const stream = await navigator.mediaDevices.getUserMedia(constraints);
-    video.srcObject = stream;
-
-    return new Promise(resolve => {
-      video.onloadedmetadata = () => {
-        resolve(video)
-      };
-    });
+  if ("fist" == resultName) {
+    document.getElementById("rock").click();
+    console.log(count++);
   }
-
-  function drawPoint(ctx, x, y, r, color) {
-    ctx.beginPath();
-    ctx.arc(x, y, r, 0, 2 * Math.PI);
-    ctx.fillStyle = color;
-    ctx.fill();
+  if ("four_up" == resultName) {
+    document.getElementById("paper").click();
+    console.log(count++);
   }
+}
 
-  function scrollWindow(x) {
-    if ('thumbs_down' == x) {
-      window.scrollBy(0, 100);
-    }
-    if ('thumbs_up' == x) {
-      window.scrollBy(0, -100);
-    }
+function scrollWindow(x) {
+  if ("thumbs_down" == x) {
+    window.scrollBy(0, 100);
   }
+  if ("thumbs_up" == x) {
+    window.scrollBy(0, -100);
+  }
+}
 
-  size = 1;
-  function zoomWindow(x) {
-    if ('finger_left' == x && size > 0.5) {
+size = 1;
+function zoomWindow(x) {
+  if ("finger_left" == x && size > 0.5) {
     size -= 0.05;
     document.body.style.zoom = size;
   }
-    if ('finger_right' == x && size < 1.5) {
+  if ("finger_right" == x && size < 1.5) {
     size += 0.05;
     document.body.style.zoom = size;
   }
 }
-  window.addEventListener("DOMContentLoaded", () => {
-
-    initCamera(
-      config.video.width, config.video.height, config.video.fps
-    ).then(video => {
+window.addEventListener("DOMContentLoaded", () => {
+  initCamera(config.video.width, config.video.height, config.video.fps).then(
+    (video) => {
       video.play();
-      video.addEventListener("loadeddata", event => {
+      video.addEventListener("loadeddata", (event) => {
         console.log("Camera is ready");
         main();
       });
-    });
+    }
+  );
 
-    const canvas = document.querySelector("#pose-canvas");
-    canvas.width = config.video.width;
-    canvas.height = config.video.height;
-    console.log("Canvas initialized");
-  });
+  const canvas = document.querySelector("#pose-canvas");
+  canvas.width = config.video.width;
+  canvas.height = config.video.height;
+  console.log("Canvas initialized");
+});
